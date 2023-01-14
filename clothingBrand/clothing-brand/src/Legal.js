d92f742b4b0b19155233a218cc02f2cb9ae2be81
Mrmarley2 <63900560+Mrmarley2@users.noreply.github.com>
2023-01-14 17:35:20 +0000

    Added Legal, Profile and Shopping sections

---
+++ clothingBrand/clothing-brand/src/Legal.js
@@ -0,0 +1,58 @@
+// legal component output
+// filler text generated at https://hipsum.co/
+export default function Legal() {
+    return(
+        <div className="container">
+            <div className="legal-section top-space">
+                <p>
+                    I'm baby mlkshk tattooed butcher vaporware twee, 
+                    williamsburg bicycle rights kombucha coloring book 
+                    prism XOXO meh vinyl. Semiotics yes plz readymade 
+                    next level schlitz cronut yuccie organic pop-up 
+                    enamel pin big mood quinoa 90's bicycle rights vice. 
+                    Hell of migas plaid, deep v woke snackwave pour-over 
+                    man bun prism. Fixie 8-bit brunch twee, meggings 
+                    selvage VHS squid raclette craft beer affogato. Tofu 
+                    artisan ugh, crucifix irony art party gluten-free 
+                    gatekeep heirloom quinoa hot chicken taxidermy snackwave
+                    thundercats craft beer. Jianbing asymmetrical mukbang 
+                    schlitz fingerstache, taxidermy etsy affogato. Ennui 
+                    hot chicken semiotics, adaptogen stumptown cliche 
+                    raw denim wayfarers 8-bit vinyl neutra sriracha.
+                </p>
+                <p>
+                    Raw denim snackwave disrupt salvia cred. Gentrify 
+                    aesthetic heirloom ramps banh mi fanny pack bruh yes 
+                    plz cold-pressed beard knausgaard vaporware hell of. 
+                    Humblebrag slow-carb vape everyday carry cardigan enamel 
+                    pin man bun la croix meh. Shaman forage butcher edison 
+                    bulb mumblecore gochujang 90's tumeric helvetica, 
+                    mlkshk poutine woke tacos gluten-free farm-to-table.
+                </p>
+                <p>
+                    Cardigan taxidermy tattooed synth vibecession keffiyeh 
+                    DIY. Street art cray offal freegan fam, dreamcatcher 
+                    migas pinterest vexillologist leggings. Paleo raclette 
+                    lumbersexual coloring book bodega boys helvetica praxis 
+                    PBR&B next level 8-bit schlitz. Marfa yr man bun PBR&B, 
+                    tacos scenester literally farm-to-table fashion axe praxis 
+                    glossier 3 wolf moon la croix sartorial.
+                </p>
+                <p>
+                    Whatever artisan art party sartorial twee lyft four loko 
+                    pork belly tumeric cronut freegan enamel pin pour-over 
+                    organic. Distillery normcore hell of kinfolk craft beer 
+                    tote bag vaporware. Trust fund small batch skateboard 
+                    seitan bruh live-edge ennui. Bruh blog iPhone, humblebrag 
+                    jianbing organic polaroid small batch tacos thundercats 
+                    Brooklyn mixtape tumeric skateboard. Tilde direct trade 
+                    polaroid, subway tile taxidermy seitan squid thundercats 
+                    butcher. Blog tumblr pork belly, migas master cleanse flannel 
+                    yes plz forage wayfarers hot chicken lo-fi single-origin 
+                    coffee pok pok austin.
+                </p>
+            </div>
+        </div>
+        
+    )            
+}
---
+++ clothingBrand/clothing-brand/src/Shopping.js
@@ -0,0 +1,20 @@
+import Product from './Product'
+
+export default function Shopping() {
+    return (
+      <div className="container">
+        <div className="product-container-shopping top-space">
+            <div className="product-container-shopping-row shopping-row-1">
+                <Product img="./images/cardigan1.jpg" name="Brown classic" type="Cardigan" cost="£80"/>
+                <Product img="./images/cardigan2.jpg" name="Cream modern" type="Cardigan" cost="£100"/>
+                <Product img="./images/cardigan3.jpg" name="White classic" type="Cardigan" cost="£120"/>
+            </div>
+            <div className="product-container-shopping-row shopping-row-2">
+                <Product img="./images/cardigan4.jpg" name="Cream modern" type="Pullover" cost="£90"/>
+                <Product img="./images/cardigan5.jpg" name="Grey modern" type="Fleece" cost="£110"/>
+                <Product img="./images/cardigan6.jpg" name="Red classic" type="Pullover" cost="£130"/>
+            </div>          
+        </div>
+      </div>      
+    );
+  }
