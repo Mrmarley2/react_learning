2f86db5673dd66fc14606e447079f83b95920b91
Mrmarley2 <63900560+Mrmarley2@users.noreply.github.com>
2023-01-22 11:25:03 +0000

    Example redux app as per course notes

---
+++ reduxreactapplication/src/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter";
+
+// The configureStore function will automatically set up an empty store for you
+// with the relevant settings you will need in the future.
+export default configureStore({
+    reducer: {
+        counter: counterReducer,
+    },
+});
