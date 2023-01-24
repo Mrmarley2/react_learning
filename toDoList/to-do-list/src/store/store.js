482c5afcf24dc34a5ed1eda1fc3751ae623e0b74
Mrmarley2 <63900560+Mrmarley2@users.noreply.github.com>
2023-01-24 19:03:09 +0000

    Updated part 2 of task 51 for submission

--- toDoList/to-do-list/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import toDoReducer from "../components/toDo";
-
-// setting up the empty store for form use
-export default configureStore({
-    reducer: {
-        toDo: toDoReducer,
-    },
-});
